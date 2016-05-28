6a1cf22e883a3d125e3547cf0c2562725fa2cccd
Max Franke <m.franke@campus.tu-berlin.de>
2016-05-28 22:49:10 +0200

    fixed csrf-token, registered namespace "tim_app"

---
+++ tim_app/static/tim_app/python/offer_request.py
@@ -0,0 +1,8 @@
+from django.shortcuts import get_object_or_404,render
+from dateutil.parser import parse
+
+def add_offer(request):
+    pass
+
+def add_request(request):
+    pass
--- tim_app/urls.py
+++ tim_app/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'tim_app'
 urlpatterns = [
     url(r'^login$', views.login, name='login'),
+    url(r'^request$', views.request, name='request'),
+    url(r'^offer$', views.offer, name='offer'),         
 ]
