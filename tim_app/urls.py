e6bd0c5aad59a31613bc8ef74fd11f9272001a82
Leonardo Hahn <leo_lh324@hotmail.com>
2016-05-29 18:29:53 +0200

    added redirect from tim/ to tim/login

--- tim_app/urls.py
+++ tim_app/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.http import HttpResponseRedirect
 
 from . import views
 
 app_name = 'tim_app'
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('login')),
     url(r'^login$', views.login, name='login'),
     url(r'^ranking$', views.ranking, name='ranking'),
     url(r'^request$', views.request, name='request'),
