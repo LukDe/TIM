80a83926606231766571469f029892583791960a
nik0vski <dominik1008@hotmail.de>
2016-05-29 04:43:20 +0200

    Add files via upload
    listenansicht über suche/biete/userdaten

--- tim_app/views.py
+++ tim_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'tim_app/login.html')
+
+def list(request):
+	return render(request, 'tim_app/list.html')
