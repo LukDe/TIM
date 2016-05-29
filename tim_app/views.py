001d8be731a1dec3aa08e8dca1d7223249159cb9
Max Franke <m.franke@campus.tu-berlin.de>
2016-05-29 14:48:48 +0200

    Added Modals error handling

--- tim_app/views.py
+++ tim_app/views.py
@@ -3,20 +3,18 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from .static.tim_app.python import offer_request as ofre 
 
-# Create your views here.
+# Create your views here..
 def login(request):
     return render(request, 'tim_app/login.html')
     
 def offer(request):
     if request.method == 'POST':
-        ofre.add_offer(request)
-        return HttpResponseRedirect(reverse('tim_app:login'))
+        return ofre.add_offer(request)
     else:
         return render(request, 'tim_app/offer.html')
 
 def request(request):
     if request.method == 'POST':
-        ofre.add_request(request)
-        return HttpResponseRedirect(reverse('tim_app:login'))
+        return ofre.add_request(request)
     else:
         return render(request, 'tim_app/request.html')
