277226e94a7454e1ad5ee75f330ba56154bcb9d6
Jessika R <jrockel@Jessikas-MacBook-Air.local>
2016-06-11 15:57:25 +0200

    rudimentary implementation for user delete, still needs testing

--- api/static/user_delete.py
+++ api/static/user_delete.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from tim_app.models import User, Request, Supply
 
 #test method can be deleted in the future
 def hello():
     print('test')
 
-def delete_user():
-    i=0
+def delete_user(username):
+    # i=0 ??
+    # get object from primary key
+    user = User.objects.get(username = username)
+    # delete all offers with that username
+    Supply.objects.get(username = user).delete()
+    # delete all requests with that username
+    Request.objects.get(username = user).delete()
+    # delete user object
+    user.delete()
+
+    # muss noch getestet werden!!
