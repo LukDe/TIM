f00111c6db0505c8fd552bde0271f7a6090bc15b
kamil.balitzki <kamil.balitzki@campus.tu-berlin.de>
2016-06-29 10:49:03 +0200

    check phone number added

--- api/static/new_user.py
+++ api/static/new_user.py
@@ -5,11 +5,14 @@ from api.static import passGen
 def create_user_phone(userName,number):
     #if username is taken, abort creation
     if User.objects.filter(username=userName):
-       print('username is already taken')
+        print('username is already taken')
+    elif User.objects.filter(phoneNr = number):
+        print('phone number already taken')
     #creates user with random password and adds it to the database
     else:
         u = User(username=userName,password=passGen.generate(),phoneNr=number)
         u.save()
+
 
 #creates a user with complete data, takes username, password, phone number, email, coordinates, radius
 def create_user_complete(userName, passw, number, mail, position, rad):
