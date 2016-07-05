c6654ec6b333cefdff0f26e8708cfd3636f62b31
Jessika R <jrockel@wlan-141-23-103-96.tubit.tu-berlin.de>
2016-07-05 11:52:11 +0200

    added sms notification

--- api/expiration.py
+++ api/expiration.py
@@ -2,6 +2,8 @@ from tim_app.models import Request
 from dateutil.parser import *
 from datetime import datetime, timedelta
 from django.utils import timezone
+from tim_app.models import User
+from sms.utils import send_sms_message
 
 def check_if_expired():
   requests = Request.objects.all().filter(active = True)
@@ -10,3 +12,4 @@ def check_if_expired():
       print (r.goodName)
       r.active = False
       r.save()
+      send_sms_message(r.username.phoneNr, "TIM: Your request for %s just expired" %(r.goodName))
