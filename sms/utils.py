c24cd78b025b9572581837427b0e9106aa703135
Leonardo Hahn <leo_lh324@hotmail.com>
2016-07-12 19:54:11 +0200

    Added format_sms_request.

--- sms/utils.py
+++ sms/utils.py
@@ -4,9 +4,41 @@ import twilio
 from twilio.rest import TwilioRestClient
 
 def send_sms_message(to_number, body):
-    client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    client = TwilioRestClient(
+        settings.TWILIO_ACCOUNT_SID,
+        settings.TWILIO_AUTH_TOKEN
+    )
     return client.messages.create(
         body = body,
         to = to_number,
         from_ = settings.TWILIO_PHONE_NUMBER,
     )
+
+def parse_sms_request(request):
+    """
+    Parses a string received as sms from the user. The function assumes
+    a format of `request/offer,goodName,quantity`. If the message is not on the
+    correct format, returns `None`, otherwise a dictionary, with keys as:
+       type, goodName, quantity
+    """
+    def error(msg):
+        return {
+            'error': msg,
+        }
+
+    formatted_req = list(map(lambda x: x.strip(), request.split(',')))
+    # The message expects three arguments.
+    if (len(formatted_req) != 3):
+        return error('Expected format: (offer/request),goodName,quantity')
+
+    if (formatted_req[0] != 'offer' and formatted_req[0] != 'request'):
+        return error('Expected format: (offer/request),goodName,quantity')
+
+    return {
+        'type': formatted_req[0],
+        'goodName': formatted_req[1],
+        'quantity': formatted_req[2],
+    }
+
+
+
